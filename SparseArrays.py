789f141e2cd1385948d671e13add53d44691430f
viikkkas <61241966+viikkkas@users.noreply.github.com>
2020-06-17 13:38:54 +0530

    Create SparseArrays.py

---
+++ SparseArrays.py
@@ -0,0 +1,47 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the matchingStrings function below.
+def matchingStrings(strings, queries):
+    res = []
+    for i in queries:
+        temp = 0
+        if i in strings:
+            for j in strings:
+                if i==j:
+                    temp = temp+1
+
+        res.append(temp)
+    
+    return res
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    strings_count = int(input())
+
+    strings = []
+
+    for _ in range(strings_count):
+        strings_item = input()
+        strings.append(strings_item)
+
+    queries_count = int(input())
+
+    queries = []
+
+    for _ in range(queries_count):
+        queries_item = input()
+        queries.append(queries_item)
+
+    res = matchingStrings(strings, queries)
+
+    fptr.write('\n'.join(map(str, res)))
+    fptr.write('\n')
+
+    fptr.close()
